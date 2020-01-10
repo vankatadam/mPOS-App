09b05269f10ed32e821aa212f905fdfff238ae72
Mahdi <mahdi.i@hotmail.de>
2020-01-10 15:17:40 +0100

    Onboarding

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,21 +1,29 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, Component } from "react";
 import {
   View,
   Text,
   ScrollView,
   SafeAreaView,
   StyleSheet,
-  Dimensions
+  Dimensions,
+  AppRegistry
 } from "react-native";
 import TutorialView from "../TutorialView";
 import { Ionicons } from "@expo/vector-icons";
 import { screenWidth } from "../screenWidth";
 import Constants from "expo-constants";
 
-export default function HomeScreen() {
+export default class HomeScreen   {
+  
+  render() {
   return (
     <View>
       <Text style={{ textAlign: "center" }}>HomeScreen</Text>
     </View>
   );
-}
+}}
+const styles = StyleSheet.create({});
+
+
+
+AppRegistry.registerComponent("Home", () => HomeScreen);
